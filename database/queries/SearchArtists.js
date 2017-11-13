8bae3a8268dc5b9196f415a3b4bcc07950235356
vijayst <vijayst@gmail.com>
2017-11-13 16:39:51 +0530

    search artists - sort only

--- database/queries/SearchArtists.js
+++ database/queries/SearchArtists.js
@@ -9,4 +9,20 @@ const Artist = require('../models/artist');
  * @return {promise} A promise that resolves with the artists, count, offset, and limit
  */
 module.exports = (criteria, sortProperty, offset = 0, limit = 20) => {
+    return Promise.all(
+        [
+            Artist.find({})
+                .sort({ [sortProperty]: 1 })
+                .skip(offset)
+                .limit(limit),
+            Artist.count()
+        ]
+    ).then(([artists, count]) => {
+        return {
+            all: artists,
+            count,
+            offset,
+            limit
+        };
+    });
 };
